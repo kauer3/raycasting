ec899857051eec80bb1a97cf886dd45e33870cd9
Kauê Reis <kaue0607@gmail.com>
2023-08-11 17:45:39 +1200

    Implement ammo system

--- Assets/Scripts/Can.cs
+++ Assets/Scripts/Can.cs
@@ -4,21 +4,50 @@ using UnityEngine;
 
 public class Can : MonoBehaviour
 {
-    float health = 100;
+    float health = .4f;
     Rigidbody rb;
+    float healthbarTimer = 0;
+    [SerializeField]
+    Canvas canvas;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        canvas = GetComponentInChildren<Canvas>();
+        canvas.enabled = false;
     }
 
     void Update()
     {
+        if (healthbarTimer > 0)
+        {
+            healthbarTimer -= Time.deltaTime;
+        }
+        else if (canvas.enabled)
+        {
+            canvas.enabled = false;
+        }
         
     }
 
-    private void Hit()
+    public void Hit(Vector3 force, float damage)
     {
-                    // rb.AddForce(-hit.normal * impactStrength);
+        if (!canvas.enabled)
+        {
+            canvas.enabled = true;
+            healthbarTimer = 5;
+        }
+
+        rb.AddForce(force);
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            RectTransform healthbar = canvas.gameObject.GetComponentsInChildren<RectTransform>()[1];
+            healthbar.sizeDelta = new Vector2(health, 0.02f);
+        }
     }
 }
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-
     private AudioSource gunAudio;
     public float impactStrength = 250f;
     public float range = 30f;
     public float fireRate = .25f;
     private float nextFire;
+    private int ammo = 5;
     private Camera cam;
+    public TMP_Text ammoIndicator;
 
     void Start()
     {
@@ -20,30 +22,31 @@ public class Gun : MonoBehaviour
 
     void Update()
     {
-        Vector3 rayOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
-        RaycastHit hit;
-
-        if (Input.GetButton("Fire1") && Time.time > nextFire) 
+        if (ammo > 0 && Input.GetButton("Fire1") && Time.time > nextFire) 
         {
+            Vector3 rayOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
             nextFire = Time.time + fireRate;
             gunAudio.Play();
+            ammo--;
+            Debug.Log(ammo);
+            ammoIndicator.text = "Ammo: " + ammo;
+
 
             if (Physics.Raycast(rayOrigin, cam.transform.forward, out hit, range))
             {
                 if (hit.rigidbody != null)
                 {
-                    hit.rigidbody.AddForce(-hit.normal * impactStrength);
+                    hit.rigidbody.GetComponent<Can>().Hit(-hit.normal * impactStrength, .1f);
                 }
             }
         }
+    }
 
-        if (Physics.Raycast(rayOrigin, cam.transform.forward, out hit, range))
-        {
-            hit.rigidbody.GetComponentInChildren<Canvas>().enabled = true;
-        }
-        else
-        {
-            hit.rigidbody.GetComponentInChildren<Canvas>().enabled = false;
-        }
+    public void Reload(int bullets)
+    {
+        ammo += bullets;
+        Debug.Log(ammo);
+        ammoIndicator.text = "Ammo: " + ammo;
     }
 }
